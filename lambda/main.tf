4f05152f7f4ce25fa7e40968dcb0f75478437b04
Zain Hasan <44829199+zainq11@users.noreply.github.com>
2024-08-15 18:22:40 -0700

    v7

--- lambda/main.tf
+++ lambda/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_object" "lambda_hello_world" {
 }
 
 resource "random_pet" "lambda_function_name" {
-  prefix = "hello-world-lambda-v6"
+  prefix = "hello-world-lambda-v7"
   length = 2
 }
 
